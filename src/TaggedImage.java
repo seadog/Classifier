1f2f2640560c280e6185413d9bda495231f32f68
Andrew Etches <andrew.etches@dur.ac.uk>
2013-03-06 11:56:22 +0000

    Convert loaded images to greyscale

--- src/TaggedImage.java
+++ src/TaggedImage.java
@@ -8,6 +8,8 @@ public class TaggedImage {
 	private Tag image_class = null;
 	private File file = null;
 	private double[] vector = null;
+	private int width = -1;
+	private int height = -1;
 	
 	public TaggedImage(Tag tag, File file){
 		this.image_class = tag;
@@ -22,6 +24,20 @@ public class TaggedImage {
 		return this.image_class;
 	}
 	
+	public int getWidth(){
+		if(this.width == -1){
+			getVector();
+		}
+		return this.width;
+	}
+	
+	public int getHeight(){
+		if(this.height == -1){
+			getVector();
+		}
+		return this.height;
+	}
+	
 	public double[] getVector(){
 		if(this.vector == null){
 			BufferedImage image = null;
@@ -33,12 +49,20 @@ public class TaggedImage {
 				return null;
 			}
 			
+			this.width = image.getWidth();
+			this.height = image.getHeight();
 			this.vector = new double[image.getWidth() * image.getHeight()];
 			
 			for(int i = 0; i < image.getWidth(); i++){
 				for(int j = 0; j < image.getHeight(); j++){
 					int rgb = image.getRGB(i, j);
-					vector[i*image.getHeight() + j] = (double)(rgb & 0xFF);
+					
+					int red = (rgb >> 16) & 0xFF;
+					int green = (rgb >> 8) & 0xFF;
+					int blue = rgb & 0xFF;
+					
+					int greyscale_value = (int)(blue*0.11) + (int)(green * 0.59) + (int)(red*0.3);
+					vector[i*image.getHeight() + j] = (double)(greyscale_value & 0xFF);
 				}
 			}
 		}
